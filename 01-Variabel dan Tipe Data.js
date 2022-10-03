27a1e5b4df065faab3829f5a497b876828edfa86
nanda <nandasafiqalfiansyah@gmail.com>
2022-10-03 07:48:06 +0700

    first commit

---
+++ 01-Variabel dan Tipe Data.js
@@ -0,0 +1,17 @@
+/* TODO:
+ * Buatlah variabel firstName, lastName, age, isMarried dengan ketentuan:
+ *  - firstName: bertipe data string, dengan nilai nama depan Anda.
+ *  - lastName: bertipe data string, dengan nilai nama belakang Anda.
+ *  - age: bertipe data number, dengan nilai umur Anda.
+ *  - isMarried: bertipe data boolean, dengan bebas Anda tentukan.
+ */
+
+let firstName =" Nanda ";
+let lastName =" Safiq ";
+let age = 19 ;
+let isMarried = false ;
+
+console.log(typeof(firstName))
+console.log(typeof(lastName))
+console.log(typeof(age))
+console.log(typeof(isMarried))
